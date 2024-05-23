8eb65a324b40b3ea809135377e4421db227224c8
Антон Філіпенко <filipenkoa270@gmail.com>
2024-05-23 13:45:53 +0300

    added my project to repository

---
+++ Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Xml.Serialization;
+using static System.Console;
+namespace Laba5
+{
+    public class App
+    {
+        private static string file_name = "C:\\Users\\Filip\\OneDrive\\Робочий стіл\\Laba5\\Result_Folder\\Zodiak_Info.txt";
+        private static string xml_name = "C:\\Users\\Filip\\OneDrive\\Робочий стіл\\Laba5\\Result_Folder\\Zodiak_Info.xml";
+        private static string json_name= "C:\\Users\\Filip\\OneDrive\\Робочий стіл\\Laba5\\Result_Folder\\Zodiak_Info.json";
+
+        private static List<Student> zodiaks = new List<Student>();
+        private static List<Student> existingZodiaks=new List<Student>();
+        private static XmlSerializer xml=new XmlSerializer(typeof(List<Student>));
+
+        public static void Choice_Sort(List<Student> zodiaks)
+        {
+            WriteLine("\nВиберіть тип сортування: ігноруючи рік чи ні");
+            string sort = ReadLine();
+            switch (sort)
+            {
+                case "не ігноруючи":
+                    zodiaks.Sort(Student.By_Year_Sort);
+                    break;
+                case "ігноруючи":
+                    zodiaks.Sort(Student.By_Month_Sort);
+                    break;
+                default:
+                    zodiaks.Sort();
+                    break;
+            }
+        }
+        public static void Adding_Student_To_List()
+        {
+            Write("Скільки хочете ввести студентів: ");
+            int length_student=int.Parse(ReadLine());
+            WriteLine("Вводьте студентів: ");
+            for(int i = 0; i < length_student; i++)
+            {
+                string line=ReadLine();
+                Student zodiak = new Student(line);
+                zodiaks.Add(zodiak);
+            }
+        }
+        public static void Add_To_Text_File(FileMode mode,List<Student> list)
+        {
+            using (FileStream file=new FileStream(file_name,mode)) {
+                using (TextWriter reader=new StreamWriter(file))
+                {
+                    for(int i = 0; i < list.Count; i++)
+                    {
+                        reader.WriteLine(list[i].sur_name + " "+list[i].name+" " + list[i].date.ToShortDateString());
+                    }
+                    
+                }
+            }
+        }
+        public static void Add_New_XML_File()
+        {
+            
+            using (FileStream file = new FileStream(file_name, FileMode.Open))
+            {
+                existingZodiaks = (List<Student>)xml.Deserialize(file);
+            }
+            existingZodiaks.AddRange(zodiaks);
+            Choice_Sort(existingZodiaks);
+            Add_To_Text_File(FileMode.Truncate, existingZodiaks);
+            using (FileStream file = new FileStream(xml_name, FileMode.Create))
+            {
+                xml.Serialize(file, existingZodiaks);
+            }
+        }
+        public static void Add_New_JSON_File()
+        {
+            string text = File.ReadAllText(json_name);
+            existingZodiaks = JsonSerializer.Deserialize<List<Student>>(text);
+            existingZodiaks.AddRange(zodiaks);
+
+            var options= new JsonSerializerOptions() { WriteIndented= true,Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping };
+            string json_string=JsonSerializer.Serialize(existingZodiaks,options);
+            File.WriteAllText(json_name, json_string);
+        }
+        public static void Create_To_XML_File()
+        {
+            using (FileStream file = new FileStream(file_name, FileMode.Create))
+            {
+                using (TextWriter writer = new StreamWriter(file))
+                {
+                    xml.Serialize(writer, zodiaks);
+                }
+            }
+
+        }
+        public static void Create_To_JSON_File()
+        {
+            var options = new JsonSerializerOptions() { WriteIndented = true, Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping };
+            string json_string = JsonSerializer.Serialize(zodiaks,options);
+            File.WriteAllText(json_name, json_string);
+        }
+
+        public static void Main()
+        {
+            string choice;
+            do
+            {
+                WriteLine("\nБажаєте доповнити чи переписати текстовий файл чи ввести вже прізвище ?");
+                choice = ReadLine();
+                switch (choice)
+                {
+                    case "доповнити":
+                        try
+                        {
+                            Adding_Student_To_List();
+                            WriteLine("Добавляємо до текстового та xml файлу");
+                            Add_New_XML_File();
+                            Add_New_JSON_File();
+                        }
+                        catch(Exception e)
+                        {
+                            WriteLine(e.Message);
+                        }
+                        break;
+                    case "переписати":
+                        try
+                        {
+                            Adding_Student_To_List();
+                            WriteLine("Добавляємо до текстового та xml файлу");
+                            Choice_Sort(zodiaks);
+                            Add_To_Text_File(FileMode.Truncate,zodiaks);
+                            Create_To_XML_File();
+                            Create_To_JSON_File();
+                        }
+                        catch(Exception e)
+                        {
+                            WriteLine(e.Message);
+                        }
+                        break;
+                    case "вивести":
+                        WriteLine("Введіть прізвище");
+                        string sur_name = ReadLine();
+                        List<Student> matchingZodiaks = new List<Student>();
+
+                        using (TextReader reader = new StreamReader(xml_name))
+                        {
+                            List<Student> result = (List<Student>)xml.Deserialize(reader);
+                            foreach (Student zodiak in result)
+                            {
+                                if (zodiak.sur_name == sur_name)
+                                {
+                                    matchingZodiaks.Add(zodiak);
+                                }
+                            }
+                        }
+                        if (matchingZodiaks.Count == 0)
+                        {
+                            WriteLine("Немає інформації для цього прізвища.");
+                        }
+                        else
+                        {
+                            foreach (var zodiak in matchingZodiaks)
+                            {
+                                WriteLine(zodiak);
+                            }
+                        }
+                        break;
+
+                }
+            } while (choice != "кінець");
+
+
+        }
+    }
+}
---
+++ ZodiakStruct.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Laba5
+{
+    [XmlRoot("zodiak")]
+    [Serializable]
+    public struct Student:IComparable<Student>
+    {
+        public static readonly List<(string, DateTime[])> list;
+
+        [XmlAttribute("sur_name")]
+        [JsonPropertyName("sur_name")]
+        public string sur_name { get; set; }
+
+        [XmlAttribute("name")]
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+
+        [XmlAttribute("date_array")]
+        [JsonPropertyName("date")]
+        public DateTime date { get; set; }
+
+        [XmlAttribute("zodiak")]
+        [JsonPropertyName("zodia")]
+        public string zodia { get; set; }
+
+        static Student()
+        {
+            List<(string, DateTime[])> Zodiaks = new List<(string, DateTime[])>();
+            Zodiaks.Add(("Aries", new DateTime[2] { new DateTime(1, 3, 21), new DateTime(1, 4, 19) }));
+            Zodiaks.Add(("Taurus", new DateTime[2] { new DateTime(1, 4, 20), new DateTime(1, 5, 20) }));
+            Zodiaks.Add(("Gemini", new DateTime[2] { new DateTime(1, 5, 21), new DateTime(1, 6, 21) }));
+            Zodiaks.Add(("Cancer", new DateTime[2] { new DateTime(1, 6, 22), new DateTime(1, 7, 22) }));
+            Zodiaks.Add(("Leo", new DateTime[2] { new DateTime(1, 7, 23), new DateTime(1, 8, 22) }));
+            Zodiaks.Add(("Virgo", new DateTime[2] { new DateTime(1, 8, 23), new DateTime(1, 9, 22) }));
+            Zodiaks.Add(("Libra", new DateTime[2] { new DateTime(1, 9, 23), new DateTime(1, 10, 23) }));
+            Zodiaks.Add(("Scorpius", new DateTime[2] { new DateTime(1, 10, 24), new DateTime(1, 11, 21) }));
+            Zodiaks.Add(("Sagittarius", new DateTime[2] { new DateTime(1, 11, 22), new DateTime(1, 12, 21) }));
+            Zodiaks.Add(("Capricornus", new DateTime[2] { new DateTime(1, 12, 22), new DateTime(1, 1, 19) }));
+            Zodiaks.Add(("Aquarius", new DateTime[2] { new DateTime(1, 1, 20), new DateTime(1, 2, 18) }));
+            Zodiaks.Add(("Pisces", new DateTime[2] { new DateTime(1, 2, 19), new DateTime(1, 3, 20) }));
+            list = Zodiaks;
+        }
+
+        public Student(string line)
+        {
+            string[] data = line.Split();
+            sur_name = data[0];
+            name = data[1];
+            date = DateTime.ParseExact(data[2], "dd.MM.yyyy", System.Globalization.CultureInfo.InvariantCulture).Date;
+            zodia = DetermineZodiak(date);
+        }
+
+        public static string DetermineZodiak(DateTime date)
+        {
+            foreach (var zodiak in list)
+            {
+                if ((date.Month == zodiak.Item2[0].Month && date.Day >= zodiak.Item2[0].Day) ||
+                    (date.Month == zodiak.Item2[1].Month && date.Day <= zodiak.Item2[1].Day))
+                {
+                    return zodiak.Item1.ToString();
+                }
+            }
+            return "Невідомо";
+        }
+
+        public override string ToString()
+        {
+            return $"{sur_name} {name} {zodia} {date.ToShortDateString()}";
+        }
+        public int CompareTo(Student other)
+        {
+            return sur_name.CompareTo(other.sur_name);
+        }
+
+        public static int By_Year_Sort(Student first, Student second)
+        {
+            return first.date.CompareTo(second.date);
+        }
+
+        public static int By_Month_Sort(Student first, Student second)
+        {
+            int monthComparison = first.date.Month.CompareTo(second.date.Month);
+            if (monthComparison == 0)
+            {
+                return first.date.Day.CompareTo(second.date.Day);
+            }
+            return monthComparison;
+        }
+    }
+}
